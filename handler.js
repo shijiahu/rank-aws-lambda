8ed392fc8d65575f96c5a4f7bdc477b83ef02e8d
Shijia Hu <shijiahu2016a@gmail.com>
2018-11-15 17:18:47 -0800

    add emoji

--- handler.js
+++ handler.js
@@ -1,13 +1,18 @@
 'use strict';
 
-module.exports.rank = async (event, context) => {
-  return {
+const emojis = ['😀','😃','😄','😁','🤩','😍','🔸','🔹','🚀','💎'];
+
+module.exports.rank = (event, context, callback) => {
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emojis[rank > emojis.length ? emojis.length - 1 : rank];
+  const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      input: rankEmoji,
     }),
   };
+  callback(null, response);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
